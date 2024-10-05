db4099ed4f45fb0317928db08dee61e8fc52b87c
xGattoMattox <149336969+xGattoMattox@users.noreply.github.com>
2024-10-05 18:09:04 +0200

    New MFD adaptation

--- tools/ini-utils/test_MY_italian.bat
+++
@@ -1,1 +0,0 @@
-node ./dist/src/index.js validate C:\Users\itali\Desktop\TraduciSC\ULTIMO_global.ini C:\Giochi\StarCitizen\StarCitizen\LIVE\data\Localization\italian_(italy)\global.ini
--- tools/ini-utils/test_italian.bat
+++
@@ -1,2 +0,0 @@
-del u:\validate.txt
-node ./dist/src/index.js validate ../../data/Localization/english/global.ini ../../data/Localization/italian_(italy)/global.ini >> u:\validate.txt
